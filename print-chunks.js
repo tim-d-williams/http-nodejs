fae8985492b14fc4a5fb825cd700cf07f1e4e6fd
Tim Williams <timwilliams.txW@gmail.com>
2019-01-14 18:58:38 +0000

    update psuedo code

--- print-chunks.js
+++ print-chunks.js
@@ -6,7 +6,8 @@ function getAndPrintHTMLChunks () {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-
+    //get the http request
+    //when the data is received print to console
     https.get(requestOptions, function (response) {
       response.setEncoding('utf8');
       response.on('data', function (data) {
@@ -16,23 +17,3 @@ function getAndPrintHTMLChunks () {
 
 }
 getAndPrintHTMLChunks()
-
-// // notice that https.get takes a callback with one parameter -
-// // response, which is a Stream that represents the HTTP response
-// https.get(requestOptions, function (response) {
-
-//   // set encoding of received data to UTF-8
-//   response.setEncoding('utf8');
-
-//   // the callback is invoked when a `data` chunk is received
-//   response.on('data', function (data) {
-//     console.log('Chunk Received. Length:', data.length);
-//   });
-
-//   // the callback is invoked when all of the data has been received
-//   // (the `end` of the stream)
-//   response.on('end', function() {
-//     console.log('Response stream complete.');
-//   });
-
-// });
