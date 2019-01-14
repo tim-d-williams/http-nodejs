0da77c5b874377a736ced29a7d3a5d2285431cf5
Tim Williams <timwilliams.txW@gmail.com>
2019-01-14 19:34:51 +0000

    create function to write results to buffer var

---
+++ print-buffer.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+    //get the http request
+    //when the data is received write to a buffer var
+    https.get(requestOptions, function (response) {
+
+      var result = '';
+      response.setEncoding('utf8');
+      response.on('data', function (data) {
+        result += data;
+      })
+      response.on('end', function(){
+        console.log(result);
+      })
+    })
+}
+getAndPrintHTML()
