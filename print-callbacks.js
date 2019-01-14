7dd3f544c8551506325523b009d67625987de7e0
Tim Williams <timwilliams.txW@gmail.com>
2019-01-14 20:15:49 +0000

    add callback to function

---
+++ print-callbacks.js
@@ -0,0 +1,27 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+
+    var result = '';
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      result += data;
+    })
+    response.on('end', function(){
+      callback(result);
+    })
+  })
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML)
